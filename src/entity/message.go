7c5b19ba48c6a3f56b5b4078aea0c03652c82807
Leonardo-Antonio <leo2001.nl08@gmail.com>
2021-09-21 15:15:41 -0500

    remove .env

--- src/entity/message.go
+++
@@ -1,7 +0,0 @@
-package entity
-
-type Message struct {
-	Name  string `json:"name,omitempty" xml:"name,omitempty"`
-	Text  string `json:"text,omitempty" xml:"text,omitempty"`
-	Email string `json:"email,omitempty" xml:"email,omitempty"`
-}
